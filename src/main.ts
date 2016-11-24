012a023a42aa2b0ba073ed9ee99c0f8b13ffd5d9
rubenCodeforges <ruben@codeforges.com>
2016-11-24 21:43:35 +0100

    templates

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone.js';
 import 'jquery';
 import 'lodash';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import '../lib/dashboard-theme.min.js';
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {AppModule} from "./app/app.module";
 
