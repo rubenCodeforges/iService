0c267fad4732d7b79890e3d52da84576b12d251a
rubenCodeforges <ruben@codeforges.com>
2016-11-24 13:36:23 +0100

    bootstrap loaders etc

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import 'core-js';
 import 'reflect-metadata';
 import 'zone.js/dist/zone.js';
+import 'jquery';
+import 'lodash';
+import 'bootstrap-loader';
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {AppModule} from "./app/AppModule";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+var fontFileLoader = 'file?name=font/[name]-[hash].[ext]';
+var imageFileLoader = 'file?name=image/[name].[ext]';
 
 module.exports = {
     entry : './src/main.ts',
@@ -9,7 +12,13 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.ts$/, loader: 'ts' }
+            {test: /\.ts$/, loader: 'ts'},
+            {test: /\.css$/, loader: ExtractTextWebpackPlugin.extract('css!autoprefixer-loader')},
+            {test: /\.less$/, loader: ExtractTextWebpackPlugin.extract('css!autoprefixer-loader!less')},
+            {test: /\.html$/, loader: 'html', exclude: /index\.html$/},
+            {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: fontFileLoader},
+            {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: fontFileLoader},
+            {test: /\.(jpe?g|png|gif|svg)$/, loader: imageFileLoader}
         ]
     },
     resolve: {
@@ -18,6 +27,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        new ExtractTextWebpackPlugin('app.[contenthash].css'),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
         })
+
     ]
 };
