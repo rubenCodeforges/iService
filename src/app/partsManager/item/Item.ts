58e2b54a9dcdba88486d7dad299ff54925a527b3
Rubenator <ruben@codeforges.com>
2017-04-06 14:53:18 +0100

    order create modal  items

--- src/app/partsManager/item/Item.ts
+++ src/app/partsManager/item/Item.ts
@@ -1,5 +1,5 @@
 export class Item {
     id: string;
-    title: string;
-    description: string;
+    title: string = "";
+    description: string = "";
 }
--- src/app/partsManager/order/modals/OrderCreateModal.ts
+++ src/app/partsManager/order/modals/OrderCreateModal.ts
@@ -1,11 +1,15 @@
 import {Component} from "@angular/core";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {Item} from "../../item/Item";
 
 @Component({
     selector: 'order-create-modal',
     template: require('./orderCreateModal.html')
 })
 export class OrderCreateModal {
+    public items: Item[] = [];
+    public newItem: Item = new Item();
+
     constructor(private activeModal: NgbActiveModal) {
     }
 
@@ -14,5 +18,13 @@ export class OrderCreateModal {
 
     public onClose() {
         this.activeModal.dismiss();
+    }
+
+    public addItem() {
+        if (!this.newItem.title) {
+            return;
+        }
+        this.items.push(this.newItem);
+        this.newItem = new Item();
     }
 }
